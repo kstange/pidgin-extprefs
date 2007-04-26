a855b4963da2096486f3ea439da49c6154791e71
kstange <kstange>
2007-04-26 10:51:25 +0000

    Major cleanup of how some things are implemented.  Hide buddy list "at signon" is now "at startup" and it works *reliably*.  I fixed up the implementation for the tooltip checkbox and eliminated some dead code.  We'll make a release tomorrow if Pidgin does.

--- src/extendedprefs.c
+++ src/extendedprefs.c
@@ -46,6 +46,7 @@
 #define KSTANGE_EP_SIZE_MIN 4
 #define KSTANGE_EP_SIZE_MAX 90
 #define KSTANGE_EP_BLIST_TIP_MIN 0
+#define KSTANGE_EP_BLIST_TIP_STD 500
 #define KSTANGE_EP_BLIST_TIP_MAX 7000
 
 static const char *pref_conv_zoom        = "/plugins/gtk/kstange/extendedprefs/conv_zoom";
@@ -53,6 +54,7 @@ static const char *pref_conv_zoom        = "/plugins/gtk/kstange/extendedprefs/c
 static const char *pref_blist_taskbar    = "/plugins/gtk/kstange/extendedprefs/blist_taskbar";
 
 static const char *pref_tooltip_delay    = "/pidgin/blist/tooltip_delay";
+static const char *pref_blist_visible    = "/pidgin/blist/list_visible";
 
 static const char *pref_conv_show_joinpart = "/plugins/gtk/kstange/extendedprefs/conv_show_joinpart";
 static const char *pref_popup_size       = "/plugins/gtk/kstange/extendedprefs/popup_size";
@@ -64,11 +66,6 @@ static const char *pref_blist_autohide   = "/plugins/gtk/kstange/extendedprefs/b
 static const char *pref_blist_tooltip		= "/plugins/gtk/kstange/extendedprefs/blist_tooltip";
 
 static gdouble _point_sizes [] = { .69444444, .8333333, 1, 1.2, 1.44, 1.728, 2.0736};
-
-static gboolean logging_in = FALSE;
-
-// Callback ids
-static guint blist_tooltip_cbid = 0;
 
 static void
 size_set(const char *widget, int value) {
@@ -235,33 +232,9 @@ blist_shrink_update(const char *pref, PurplePrefType type, gpointer value,
 }
 
 static void
-blist_show_cb(GtkWidget *blist, void *nothing)
-{
-	if(purple_prefs_get_bool(pref_blist_autohide) && logging_in == TRUE)
-		gtk_widget_hide(blist);
-}
-
-static void
 blist_created_cb(PurpleBuddyList *blist, void *data) {
-	PidginBuddyList *gtkblist = PIDGIN_BLIST(blist);
-
 	blist_taskbar_update(NULL, 0, GINT_TO_POINTER(purple_prefs_get_bool(pref_blist_taskbar)), NULL);
 	blist_shrink_update(NULL, 0, GINT_TO_POINTER(purple_prefs_get_bool(pref_blist_allow_shrink)), NULL);
-
-	g_signal_connect(G_OBJECT(gtkblist->window), "show",
-					 G_CALLBACK(blist_show_cb), NULL);
-
-	if (purple_prefs_get_bool(pref_blist_autohide) && GPOINTER_TO_INT(data) == TRUE) {
-		gtk_widget_hide(gtkblist->window);
-		logging_in = TRUE;
-	}
-}
-
-static void
-blist_signon_check_cb(PurpleConnection *gc, void *data)
-{
-	if (purple_connections_get_connecting() == NULL)
-		logging_in = FALSE;
 }
 
 static gboolean
@@ -274,41 +247,52 @@ connect_callback(PurplePlugin *plugin, const char *pref, PurplePrefCallback func
 	purple_prefs_connect_callback(plugin, pref, function, NULL);
 }
 
-// Callback showing the tooltip and tooltip reveal time
+/* Callback showing the tooltip and tooltip reveal time */
 static void
 blist_tooltip_update(const char *name, PurplePrefType type, gconstpointer value, gpointer data) {
-	GtkContainer *widget = data;
-	GtkSpinButton *spin_button;
-	gint reveal_delay;
+	GtkWidget *hbox = data;
+	GtkSpinButton *spin = g_list_last(gtk_container_get_children(GTK_CONTAINER(hbox)))->data;
 
-	if (purple_prefs_get_bool(name)) {	
-		spin_button = g_list_last(gtk_container_get_children(widget))->data;
-		reveal_delay = gtk_spin_button_get_value_as_int(spin_button);
-		purple_prefs_set_int(pref_tooltip_delay, reveal_delay);
-		gtk_spin_button_set_value(spin_button, reveal_delay);
-		gtk_widget_set_sensitive(GTK_WIDGET(widget), TRUE);
+	if (purple_prefs_get_bool(name)) {
+		gtk_widget_set_sensitive(hbox, TRUE);
+		gtk_spin_button_set_value(spin, KSTANGE_EP_BLIST_TIP_STD);
 	}
 	else {
-		gtk_widget_set_sensitive(GTK_WIDGET(widget), FALSE);
-		purple_prefs_set_int(pref_tooltip_delay, KSTANGE_EP_BLIST_TIP_MIN);
+		gtk_widget_set_sensitive(hbox, FALSE);
+		gtk_spin_button_set_value(spin, KSTANGE_EP_BLIST_TIP_MIN);
 	}
 }
 
+static void
+delete_prefs(GtkWidget *widget, void *plugin) {
+	/* Unregister callbacks. */
+	purple_prefs_disconnect_by_handle(plugin);
+}
 
 static gboolean
 plugin_load(PurplePlugin *plugin) {
 	PidginBuddyList *gtkblist = PIDGIN_BLIST(purple_get_blist());
 
-	if (gtkblist != NULL && GTK_IS_WINDOW(gtkblist->window)) {
-		blist_created_cb(purple_get_blist(), (gpointer)FALSE);
+	/* Set the buddy list pref to invisible.  If Pidgin is loading, the
+	   buddy list will still get hidden.  Otherwise, we don't want to do
+	   anything anyway. */
+	if (purple_prefs_get_bool(pref_blist_autohide)) {
+		purple_prefs_set_bool(pref_blist_visible, FALSE);
 	}
 
-	purple_signal_connect(pidgin_blist_get_handle(), "gtkblist-created", plugin, PURPLE_CALLBACK(blist_created_cb), (gpointer)TRUE);
-	purple_signal_connect(purple_connections_get_handle(), "signed-on", plugin, PURPLE_CALLBACK(blist_signon_check_cb), NULL);
+	/* If the blist already exists, activate the prefs for it. */
+	if (gtkblist != NULL && GTK_IS_WINDOW(gtkblist->window)) {
+		blist_created_cb(purple_get_blist(), NULL);
+	}
 
+	/* For when the blist is created, restore prefs for it. */
+	purple_signal_connect(pidgin_blist_get_handle(), "gtkblist-created", plugin, PURPLE_CALLBACK(blist_created_cb), NULL);
+
+	/* Handle the join and part chat signals to hide the messages if needed */
 	purple_signal_connect(purple_conversations_get_handle(), "chat-buddy-joining", plugin, PURPLE_CALLBACK(chat_join_part_cb), NULL);
 	purple_signal_connect(purple_conversations_get_handle(), "chat-buddy-leaving", plugin, PURPLE_CALLBACK(chat_join_part_cb), NULL);
 
+	/* Initialize all font size prefs. */
 	size_prefs_init_all();
 
 	/* Connect the preference callbacks we want to use. */
@@ -327,6 +311,7 @@ static gboolean
 plugin_unload(PurplePlugin *plugin) {
 	PidginBuddyList *gtkblist = PIDGIN_BLIST(purple_get_blist());
 
+	/* Disable buddy list modifications we set */
 	if (gtkblist != NULL && GTK_IS_WINDOW(gtkblist->window)) {
 		/* FIXME: In Win32, the taskbar entry won't come back till the window
 		   is focused. */
@@ -335,31 +320,45 @@ plugin_unload(PurplePlugin *plugin) {
 		GTK_WINDOW(gtkblist->window)->allow_shrink = FALSE;
 	}
 
+	/* Reset the buddy list's visible preference to its actual state */
+	if (purple_prefs_get_bool(pref_blist_autohide)) {
+		gboolean visible;
+		if (gtkblist != NULL && GTK_IS_WINDOW(gtkblist->window)) {
+			g_object_get(G_OBJECT(gtkblist->window), "visible", &visible, NULL);
+		} else {
+			visible = FALSE;
+		}
+		purple_prefs_set_bool(pref_blist_visible, visible);
+	}
+
+	/* Reset all fonts back to standard sizes. */
 	size_prefs_clear_all();
 
 	return TRUE;
 }
 
 static GtkWidget* get_config_frame(PurplePlugin *plugin) {
-	GtkWidget *ret, *hbox, *reveal_delay_widget, *nb;
+	GtkWidget *ret, *nb, *hbox, *spin, *page;
 	GtkWidget *vbox;
-	GtkWidget *tab_label1, *tab_label2, *page2;
 	GtkWidget *label;
 	GtkSizeGroup *sg;
 
-	purple_prefs_disconnect_callback(blist_tooltip_cbid);
+	ret = gtk_vbox_new(FALSE, 6);
+
 	nb = gtk_notebook_new();
-	ret = gtk_vbox_new(FALSE, 18);
-	tab_label1 = gtk_label_new("General");
-	tab_label2 = gtk_label_new("Accels");
-	page2 = gtk_vbox_new(FALSE, 18);
-	gtk_notebook_append_page(GTK_NOTEBOOK(nb), ret, tab_label1);
-	gtk_notebook_append_page(GTK_NOTEBOOK(nb), page2, tab_label2);
+	gtk_box_pack_start(GTK_BOX(ret), nb, FALSE, FALSE, 0);
 
-	gtk_container_set_border_width (GTK_CONTAINER(ret), 12);
-	gtk_container_set_border_width (GTK_CONTAINER(page2), 12);
+	/* If this frame is destroyed, clean up after it. */
+        g_signal_connect(G_OBJECT(ret), "destroy",
+                                         G_CALLBACK(delete_prefs), plugin);
 
-	vbox = pidgin_make_frame (ret, "Interface Font Sizes (points)");
+	/* Create a notebook tab for the General prefs */
+	page = gtk_vbox_new(FALSE, 18);
+	label = gtk_label_new("General");
+	gtk_notebook_append_page(GTK_NOTEBOOK(nb), page, label);
+	gtk_container_set_border_width (GTK_CONTAINER(page), 12);
+
+	vbox = pidgin_make_frame (page, "Interface Font Sizes (points)");
 
 	sg = gtk_size_group_new(GTK_SIZE_GROUP_HORIZONTAL);
 
@@ -396,38 +395,46 @@ static GtkWidget* get_config_frame(PurplePlugin *plugin) {
 		gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, FALSE, 0);
 	}
 
-	vbox = pidgin_make_frame (ret, "Conversations");
+	vbox = pidgin_make_frame (page, "Conversations");
 
 	pidgin_prefs_checkbox("Show _join and part messages in chats",
 							pref_conv_show_joinpart, vbox);
 
-	vbox = pidgin_make_frame (ret, "Buddy List");
+	vbox = pidgin_make_frame (page, "Buddy List");
 
 	/* Tooltip Delay */
 	hbox = gtk_hbox_new(FALSE, 4);
 	gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
-	pidgin_prefs_checkbox("Show buddy _tooltip:", pref_blist_tooltip, hbox);
-	reveal_delay_widget = pidgin_prefs_labeled_spin_button(hbox, "Reveal delay (ms):",
+	pidgin_prefs_checkbox("Show buddy _tooltips:", pref_blist_tooltip, hbox);
+	spin = pidgin_prefs_labeled_spin_button(hbox, "Reveal delay (ms):",
 										pref_tooltip_delay,
 										KSTANGE_EP_BLIST_TIP_MIN,
 										KSTANGE_EP_BLIST_TIP_MAX,
 										NULL);
-	gtk_widget_set_sensitive(GTK_WIDGET(reveal_delay_widget), purple_prefs_get_bool(pref_blist_tooltip));
-	blist_tooltip_cbid = purple_prefs_connect_callback(plugin, pref_blist_tooltip, 
-									blist_tooltip_update, reveal_delay_widget);
+	gtk_widget_set_sensitive(GTK_WIDGET(spin), purple_prefs_get_bool(pref_blist_tooltip));
+	purple_prefs_connect_callback(plugin, pref_blist_tooltip,
+									blist_tooltip_update, spin);
 
 	/* Window Widget Tweaking Prefs */
 	pidgin_prefs_checkbox("Show buddy _list entry in taskbar",
 							pref_blist_taskbar, vbox);
 
-	pidgin_prefs_checkbox("Hide buddy list at _signon",
+	pidgin_prefs_checkbox("Hide buddy list at _startup",
 							pref_blist_autohide, vbox);
 
 	pidgin_prefs_checkbox("Allow buddy list to s_hrink below normal size constraints",
 							pref_blist_allow_shrink, vbox);
 
+#if 0
+	/* Create a notebook tab for the Accels editor */
+	page = gtk_vbox_new(FALSE, 18);
+	label = gtk_label_new("Accels");
+	gtk_notebook_append_page(GTK_NOTEBOOK(nb), page, label);
+	gtk_container_set_border_width (GTK_CONTAINER(page), 12);
+#endif
+
 	gtk_widget_show_all(ret);
-	return nb;
+	return ret;
 }
 
 static PidginPluginUiInfo ui_info =
